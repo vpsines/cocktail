0218d418f99782623caf0e78134f941de1ccd36a
vpsines <sasip6028@gmail.com>
2025-11-13 09:22:52 +0530

    add tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import {ScrollTrigger, SplitText} from 'gsap/all';
+import gsap from 'gsap';
+
+gsap.registerPlugin({ScrollTrigger, SplitText,});
 
 const App = () => {
     return (
-        <div>App</div>
+        <div className="flex-center h-[100vh]">
+            <h1 className="text-3xl text-indigo-300">App</h1>
+        </div>
     )
 }
 export default App
