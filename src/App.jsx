c181539ea3915fbd1e249ccad6c108491eb993e4
vpsines <sasip6028@gmail.com>
2025-11-13 12:07:13 +0530

    feat:add hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import {ScrollTrigger, SplitText} from 'gsap/all';
 import gsap from 'gsap';
 import Navbar from "./components/Navbar.jsx";
+import Hero from "./components/Hero.jsx";
 
-gsap.registerPlugin({ScrollTrigger, SplitText,});
+gsap.registerPlugin(ScrollTrigger, SplitText,);
 
 const App = () => {
     return (
         <main>
             <Navbar/>
+            <Hero/>
+            <div className="h-dvh bg-black">
+
+            </div>
         </main>
     )
 }
